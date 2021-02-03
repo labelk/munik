740d2e903331ca3cc7dc5ac8d4ee724b28feb13d
labelk <78467518+labelk@users.noreply.github.com>
2021-02-03 15:15:16 +0700

    Update extraSettings.js

--- src/extraSettings.js
+++ src/extraSettings.js
@@ -35,7 +35,7 @@ let fruitSlowDown = false;
 let clickChangeFruit = false;
 
 // 广告链接：false 或为空字符串表示不会跳转到广告
-let adLink = 'https://636f-codenav-8grj8px727565176-1256524210.tcb.qcloud.la/yupi_wechat.png';
+let adLink = 'https://twitter.com/DONGKIZTOWN';
 
 // 修改网页标题：将 "큐티문익!" 进行替换
 document.getElementsByTagName("title")[0].innerText = '큐티문익!';
